ccf4d925bc4ed9bba6817614e1997243de669bfe
Aleks <a.ivanov.developer@gmail.com>
2024-07-11 16:46:12 +0100

    Added mobile styling to footer.

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -13,12 +13,10 @@ export default function FooterComponent() {
     let pathname = usePathname()
     const checkPath = pathname === "/"
     return (
-        <footer>
-            <div className={`${checkPath ? "bg-allmost-black text-white" : "bg-gold text-allmost-black"}`}>
+        <footer className={`${checkPath ? "bg-allmost-black text-white" : "bg-gold text-allmost-black"} h-[375px] flex flex-col justify-around px-8`}>
                 <LogoComponent dark={checkPath}/>
                 <p>The Modern Art Gallery is free to all visitors and open seven days a week from 8am to 9pm. Find us at 99 King Street, Newport, USA.</p>
                 <SocialComponent dark={checkPath}/>
-            </div>
         </footer>
     )
 }
